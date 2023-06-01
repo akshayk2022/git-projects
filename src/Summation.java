28568179cdda9868fefb938c842afb0348f3513f
akshayk2022 <www.akshaykumar04855@gmail.com>
2023-06-01 23:56:32 +0530

    initial commit

---
+++ src/Summation.java
@@ -0,0 +1,24 @@
+public class Summation {
+
+    public void foo(int s, int k) {
+        int c;
+        if (s > k) {
+            c = s + k;
+            System.out.println(c);
+        } else if (s < k) {
+            c = s - k;
+            System.out.println(c);
+        } else {
+            c = s * k;
+            System.out.println(c);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Summation v= new Summation();
+        v. foo(5,4);
+
+
+    }
+}
